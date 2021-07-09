399511c0dd1014af1ad3dc526dc670552d0bc92d
Stackbit Code Editor <projects@stackbit.com>
2021-07-09 20:30:29 +0000

    GridItem.js: changed

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -90,7 +90,7 @@ export default class GridItem extends React.Component {
                                         'mt-3': imagePosition === 'bottom' && hasText && (!isCard || hasImagePadding)
                                     })}
                                 >
-                                    <img class="icon"
+                                    <icon
                                         src={withPrefix(image)}
                                         alt={imageAlt}
                                         className={classNames({
